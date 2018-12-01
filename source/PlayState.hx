bf9b2a4cec9457e4e59a751d0d1405d39df2bd5e
Mike <mjahearn13@alum.mit.edu>
2018-11-30 22:41:26 -0500

    collision has to happen first now???

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,52 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.addons.editors.tiled.TiledMap;
+import flixel.addons.editors.tiled.TiledTileLayer;
+import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState
 {
+	var player:FlxSprite;
+	var level:FlxTilemap;
+	
 	override public function create():Void
 	{
+		var levelData:TiledMap = new TiledMap("assets/data/testmap.tmx");
+		var someLayer:TiledTileLayer = cast(levelData.getLayer("main"), TiledTileLayer);
+		
+		level = new FlxTilemap();
+		level.loadMapFromCSV(someLayer.csvData, "assets/images/testsheet.png", 20, 20, 1, 1, 2);
+		add(level);
+		
+		player = new FlxSprite(20, 320);
+		player.makeGraphic(20, 20, 0xff0000ff);
+		player.acceleration.y = 500;
+		player.drag.x = 500;
+		player.maxVelocity.x = 250;
+		player.maxVelocity.y = 1000;
+		add(player);
+		
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		FlxG.collide(player,level);
+		
+		player.acceleration.x = 0;
+		if (FlxG.keys.pressed.RIGHT) {
+			player.acceleration.x = 500;
+		}
+		if (FlxG.keys.pressed.LEFT) {
+			player.acceleration.x = -500;
+		}
+		if (FlxG.keys.justPressed.UP && player.isTouching(FlxObject.FLOOR)) {
+			player.velocity.y = -250;
+		}
 		super.update(elapsed);
 	}
 }
