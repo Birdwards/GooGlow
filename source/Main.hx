8ef587494befed1b8cea7dbfe13f3beccf64bbec
Mike <mjahearn13@alum.mit.edu>
2018-11-30 22:04:44 -0500

    basic setup

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,10 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		
+		addChild(new FlxGame(640, 360, PlayState, true, true));
+		
+		FlxG.autoPause = false;
+		FlxG.mouse.useSystemCursor = true;
 	}
 }
