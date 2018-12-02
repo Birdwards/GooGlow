394eecf64e046540853114e99d87b69b782a8408
Mike <mjahearn13@alum.mit.edu>
2018-12-01 21:26:46 -0500

    added exit, tilesheet, randomized splash particle size

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.addons.editors.tiled.TiledLayer;
 import flixel.addons.editors.tiled.TiledMap;
 import flixel.addons.editors.tiled.TiledObjectLayer;
 import flixel.addons.editors.tiled.TiledTileLayer;
@@ -11,6 +12,7 @@ import flixel.effects.particles.FlxEmitter;
 import flixel.group.FlxGroup;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
+import flixel.tile.FlxBaseTilemap;
 import flixel.tile.FlxTilemap;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
@@ -25,7 +27,7 @@ class PlayState extends FlxState
 {
 	static var GRAVITY:Float = 500;
 	static var SPLAT_VEL:Float = 250;
-	static var NUM_LIVES:Int = 5;
+	static var NUM_LIVES:Int = 20;
 	
 	var maskRect:Rectangle;
 	var startPoint:FlxPoint;
@@ -35,6 +37,7 @@ class PlayState extends FlxState
 	var player:FlxSprite;
 	var splatEmitter:FlxEmitter;
 	
+	var levelData:TiledMap;
 	var level:FlxTilemap;
 	var levelMask:FlxSprite;
 	var nullSprite:FlxSprite;
@@ -46,16 +49,18 @@ class PlayState extends FlxState
 	var platforms:FlxTypedGroup<FlxSprite>;
 	var curPlatform:FlxSprite;
 	
+	var exit:FlxSprite;
+	
 	override public function create():Void
 	{		
 		respawnTimer = new FlxTimer();
 		livesLeft = NUM_LIVES;
 		
-		var levelData:TiledMap = new TiledMap("assets/data/testmap.tmx");
+		levelData = new TiledMap("assets/data/level1.tmx");
 		var someLayer:TiledTileLayer = cast(levelData.getLayer("main"), TiledTileLayer);
 		
 		level = new FlxTilemap();
-		level.loadMapFromCSV(someLayer.csvData, "assets/images/testsheet.png", 20, 20, 1, 1, 1);
+		level.loadMapFromCSV(someLayer.csvData, "assets/images/tilesheet.png", 20, 20, FlxTilemapAutoTiling.FULL, 1, 1, 1);
 		add(level);
 		
 		levelMask = new FlxSprite();
@@ -69,60 +74,81 @@ class PlayState extends FlxState
 		nullObs = new FlxSprite().makeGraphic(20, 20, 0xffff0000);
 		
 		obstacles = new FlxTypedGroup();
-		var obstacleLayer:TiledObjectLayer = cast(levelData.getLayer("obstacles"), TiledObjectLayer);
-		var obstaclePathLayer:TiledObjectLayer = cast(levelData.getLayer("obstaclePaths"), TiledObjectLayer);
-		for (op in obstaclePathLayer.objects) {
-			for (p in op.points) {
-				p.x += op.x;
-				p.y += op.y;
-			}
-		}
-		for (o in obstacleLayer.objects) {
-			var newObs:FlxSprite = new FlxSprite(o.x, o.y);
-			newObs.makeGraphic(20, 20, 0xff000000);
-			newObs.immovable = true;
+		var obstacleLayer:TiledObjectLayer = getObjectLayer("obstacles");
+		var obstaclePathLayer:TiledObjectLayer = getObjectLayer("obstaclePaths");
+		if (obstaclePathLayer != null) {
 			for (op in obstaclePathLayer.objects) {
-				if (op.name == o.name) {
-					FlxTween.linearPath(newObs, op.points, 100, false, {ease:FlxEase.sineInOut, type:FlxTweenType.PINGPONG});
-					break;
+				for (p in op.points) {
+					p.x += op.x;
+					p.y += op.y;
 				}
 			}
-			obstacles.add(newObs);
 		}
+		if (obstacleLayer != null) {
+			for (o in obstacleLayer.objects) {
+				var newObs:FlxSprite = new FlxSprite(o.x, o.y);
+				newObs.makeGraphic(20, 20, 0xff000000, true);
+				newObs.immovable = true;
+				if (obstaclePathLayer != null) {
+					for (op in obstaclePathLayer.objects) {
+						if (op.name == o.name) {
+							FlxTween.linearPath(newObs, op.points, 100, false, {ease:FlxEase.sineInOut, type:FlxTweenType.PINGPONG});
+							break;
+						}
+					}
+				}
+				obstacles.add(newObs);
+			}
+		}		
 		add(obstacles);
 		
 		nullPlat = new FlxSprite().makeGraphic(20, 20, 0xff0000ff);
 		
 		platforms = new FlxTypedGroup();
-		var platformLayer:TiledObjectLayer = cast(levelData.getLayer("platforms"), TiledObjectLayer);
-		var platformPathLayer:TiledObjectLayer = cast(levelData.getLayer("platformPaths"), TiledObjectLayer);
-		for (pp in platformPathLayer.objects) {
-			for (p in pp.points) {
-				p.x += pp.x;
-				p.y += pp.y;
-			}
-		}
-		for (p in platformLayer.objects) {
-			var newPlat:FlxSprite = new FlxSprite(p.x, p.y);
-			newPlat.makeGraphic(60, 10, 0xff000000);
-			newPlat.immovable = true;
+		var platformLayer:TiledObjectLayer = getObjectLayer("platforms");
+		var platformPathLayer:TiledObjectLayer = getObjectLayer("platformPaths");
+		if (platformPathLayer != null) {
 			for (pp in platformPathLayer.objects) {
-				if (pp.name == p.name) {
-					FlxTween.linearPath(newPlat, pp.points, 50, false, {ease:FlxEase.sineInOut, type:FlxTweenType.PINGPONG});
-					break;
+				for (p in pp.points) {
+					p.x += pp.x;
+					p.y += pp.y;
 				}
 			}
-			platforms.add(newPlat);
 		}
+		if (platformLayer != null) {
+			for (p in platformLayer.objects) {
+				var newPlat:FlxSprite = new FlxSprite(p.x, p.y);
+				newPlat.makeGraphic(60, 10, 0xff000000, true);
+				newPlat.immovable = true;
+				if (platformPathLayer != null) {
+					for (pp in platformPathLayer.objects) {
+						if (pp.name == p.name) {
+							FlxTween.linearPath(newPlat, pp.points, 50, false, {ease:FlxEase.sineInOut, type:FlxTweenType.PINGPONG});
+							break;
+						}
+					}
+				}			
+				platforms.add(newPlat);
+			}
+		}		
 		add(platforms);
 		
 		curPlatform = null;
 		
-		var playerLayer:TiledObjectLayer = cast(levelData.getLayer("player"), TiledObjectLayer);
+		var exitLayer:TiledObjectLayer = getObjectLayer("exit");
+		exit = new FlxSprite(exitLayer.objects[0].x, exitLayer.objects[0].y);
+		exit.makeGraphic(20, 20, 0xffffffff);
+		add(exit);
+		
+		var playerLayer:TiledObjectLayer = getObjectLayer("player");
 		startPoint = new FlxPoint(playerLayer.objects[0].x, playerLayer.objects[0].y);
 		
 		player = new FlxSprite(startPoint.x, startPoint.y);
 		player.makeGraphic(20, 20, 0xff0080ff);
+		player.width = 16;
+		player.height = 16;
+		player.offset.x = 2;
+		player.offset.y = 4;
 		player.acceleration.y = GRAVITY;
 		player.drag.x = 500;
 		player.maxVelocity.x = 250;
@@ -131,7 +157,10 @@ class PlayState extends FlxState
 		
 		splatEmitter = new FlxEmitter(player.x, player.y);
 		splatEmitter.setSize(player.width, player.height);
-		splatEmitter.makeParticles(4, 4, 0xff00ff00, 30);
+		//splatEmitter.makeParticles(4, 4, 0xff00ff00, 30);
+		for (i in 0...30) {
+		  splatEmitter.add(new SplatParticle());
+		}
 		splatEmitter.launchMode = FlxEmitterMode.SQUARE;
 		splatEmitter.acceleration.set(0,GRAVITY);
 		splatEmitter.solid = true;
@@ -153,6 +182,7 @@ class PlayState extends FlxState
 		FlxG.collide(player, level);
 		FlxG.collide(player, platforms, function(a:FlxSprite, b:FlxSprite){if (b.isTouching(FlxObject.UP)) {curPlatform = b;}});
 		FlxG.collide(player, obstacles, function(a:FlxSprite, b:FlxSprite){killPlayer();});
+		FlxG.overlap(player, exit, exitLevel);
 		FlxG.collide(splatEmitter, level, drawSplatOnLevel);
 		FlxG.collide(splatEmitter, platforms, drawSplatOnSprite(nullPlat));
 		FlxG.collide(splatEmitter, obstacles, drawSplatOnSprite(nullObs));
@@ -251,5 +281,23 @@ class PlayState extends FlxState
 			}
 			a.kill();
 		}
+	}
+	
+	function getObjectLayer(name:String):TiledObjectLayer {
+		var layer:TiledLayer = levelData.getLayer(name);
+		if (layer == null) {
+			return null;
+		}
+		return cast(layer, TiledObjectLayer);
+	}
+	
+	function exitLevel(a:FlxSprite, b:FlxSprite):Void {
+		if (a.x >= b.x && a.x + a.width <= b.x + b.width && a.y >= b.y && a.y + a.height <= b.y + b.height) {
+			a.kill();
+			//todo: increment level
+			respawnTimer.start(1, function(t:FlxTimer) {
+				FlxG.resetState();
+			});
+		}		
 	}
 }
---
+++ source/SplatParticle.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.effects.particles.FlxParticle;
+import flixel.math.FlxRandom;
+
+class SplatParticle extends FlxParticle {
+	
+	static var rando:FlxRandom;
+	
+	public function new() {
+		super();
+		if (rando == null) {
+			rando = new FlxRandom();
+		}
+		var randSize:Int = rando.int(1, 8);
+		makeGraphic(randSize, randSize, 0xff80ff00);
+	}
+}
